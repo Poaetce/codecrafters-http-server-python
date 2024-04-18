4ea706279496a2071c0e8dce8853dc00515c7d30
Poaetce <poaetce@gmail.com>
2024-04-18 12:10:16 +0900

    3

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 import socket
 
 
+class Request:
+    def __init__(self, request: bytes) -> None:
+        lines: list[str] = request.decode().splitlines()
+
+        self.http_method: str
+        self.path: str
+        self.http_version: str
+        self.http_method, self.path, self.http_version = lines[0].split(' ')
+
+        self.headers: list[str] = lines[1:]
+
+
 def main() -> None:
     server_socket: socket.socket = socket.create_server(('localhost', 4221), reuse_port=True)
     connection: socket.socket
@@ -8,8 +20,14 @@ def main() -> None:
     connection, address = server_socket.accept()
 
     with connection:
-        data: bytes = connection.recv(1024)
-        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+        request: Request = request(connection.recv(1024))
+        
+        if request.path == '/':
+            response: str = "HTTP/1.1 200 OK\r\n\r\n"
+        else:
+            response: str = "HTTP/1.1 404 Not Found\r\n\r\n"
+
+        connection.sendall(response.encode())
 
 
 if __name__ == "__main__":
