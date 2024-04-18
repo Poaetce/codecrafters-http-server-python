c989a34f033912b98f01678eed761f74b4dd0971
Poaetce <poaetce@gmail.com>
2024-04-18 11:47:50 +0900

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 import socket
 
 
-def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+def main() -> None:
+    server_socket: socket.socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    connection: socket.socket
+    address: tuple[str, int]
+    connection, address = server_socket.accept()
+
+    with connection:
+        data: bytes = connection.recv(1024)
+        connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
