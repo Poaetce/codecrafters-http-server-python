a0384ac6550d30662f2245f243f3c94cd1318548
Poa <poaetcethe2nd@gmail.com>
2024-04-26 20:54:45 +0900

    pass stage

--- app/main.py
+++ app/main.py
@@ -23,6 +23,7 @@ class Request:
 
 def respond(status_code: int, content: str | None = None, content_type: str | None = None) -> str:
     CRLF: str = '\r\n'
+    print('hi')
     
     status_line: str
     headers: list[str] = []
@@ -40,8 +41,6 @@ def respond(status_code: int, content: str | None = None, content_type: str | No
 
         body =  CRLF + content +  CRLF
 
-
-    print(CRLF.join([status_line,CRLF.join(headers),body]))
     return CRLF.join([status_line,CRLF.join(headers),body])
 
 
